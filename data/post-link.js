61236b41b62ec0fd36ac3b43c07083f5bb243d18
Daniel Lombraña González <teleyinex@gmail.com>
2013-10-04 10:55:50 +0200

    Layout fixes

--- data/post-link.js
+++ data/post-link.js
@@ -4,13 +4,15 @@ self.on("message", function(project){
         var img_id = "img_" + String(i);
         var img = $("<img/>", {id: img_id});
         img.attr("src", project.images[i]);
-        img.css("max-width", "250px");
+        img.css("width", "100%");
+        img.css("overflow", "hidden");
         //Create the li item
         var li = $("<li/>", {id: "photo_" + img_id});
         li.addClass("span12");
         // Create the div element
         var div = $("<div/>");
         div.addClass("thumbnail");
+        div.css("background-color", "white");
         // Add the image to the div element
         div.append(img);
         // Create the image caption with the buttons
@@ -18,8 +20,12 @@ self.on("message", function(project){
         caption.addClass("caption");
         // Create a paragraph to host the buttons
         var p = $("<p/>");
-        var btn = $("<button/>", {id: "btn_" + img_id});
+        p.css("text-align", "center");
+        var btn = $("<button/>", {id: "btn_" + img_id, class: 'btn'});
         btn.text("Send it!");
+        // Button group
+        var btn_group = $("<div/>");
+        btn_group.attr("class", "btn-group");
         // Button action
         var img_url = project.images[i];
         btn.off('click').on('click', function(){
@@ -31,8 +37,9 @@ self.on("message", function(project){
             $("#photo_" + tmp).hide();
             $.post("http://links.com:5000", {url: img_url, project_slug: project.slug}); 
         });
-        p.append(btn);
-        var btnDiscard = $("<button/>", {id: "btnDiscard_" + img_id});
+        //p.append(btn);
+        btn_group.append(btn);
+        var btnDiscard = $("<button/>", {id: "btnDiscard_" + img_id, class:'btn'});
         btnDiscard.text("Discard it!");
         btnDiscard.off('click').on('click', function(){
             // Get image id removing btn_ string
@@ -42,7 +49,9 @@ self.on("message", function(project){
             console.log(img_url);
             $("#photo_" + tmp).hide();
         });
-        p.append(btnDiscard);
+        //p.append(btnDiscard);
+        btn_group.append(btnDiscard);
+        p.append(btn_group);
         caption.append(p);
         div.append(caption);
         li.append(div);
